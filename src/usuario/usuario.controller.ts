1b96641bc107db70432211ad20ccfd83ada4b532
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-11-22 13:37:58 -0300

    isolating according to modules rules

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -7,7 +7,7 @@ export class UsuarioController{
     private usuarioRepository = new Usuariorepository()
 
     @Post()
-    async criarUsuario(@Body() dadosUsuario = {}){
+    async criarUsuario(@Body() dadosUsuario){
         this.usuarioRepository.salvar(dadosUsuario);
         return dadosUsuario;
     }
