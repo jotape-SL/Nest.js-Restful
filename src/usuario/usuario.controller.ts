34a16e4cbb8b985942e93b12092277a5327efe96
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-11-25 15:39:09 -0300

    user entity

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,18 +1,27 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { CriaUsuarioDOT } from "./dto/criaUsuario.dts";
+import { UsuarioEntity } from "./usuario.entity";
 import { Usuariorepository } from "./usuario.repository";
+import {v4 as uuid} from 'uuid';
 
 @Controller('/usuarios')
 export class UsuarioController{
 
-    constructor(private usuarioRepository: Usuariorepository){
-        //
-    }
+    constructor(private usuarioRepository: Usuariorepository){}
 
     @Post()
     async criarUsuario(@Body() dadosUsuario : CriaUsuarioDOT){
-        this.usuarioRepository.salvar(dadosUsuario);
-        return dadosUsuario;
+        const usuarioEntity = new UsuarioEntity();
+        usuarioEntity.email = dadosUsuario.email
+        usuarioEntity.senha = dadosUsuario.senha
+        usuarioEntity.nome = dadosUsuario.nome
+        usuarioEntity.id = uuid();
+
+        this.usuarioRepository.salvar(usuarioEntity);
+        return {
+          id: usuarioEntity.id, 
+          message:'Usuario criado'
+        }
     }
 
     @Get()
